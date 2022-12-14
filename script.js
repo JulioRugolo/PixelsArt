2ddbd6adb40359fc4e2b28d79b2624448ecd7608
Julio Rugoloe <juliorugolo@gmail.com>
2022-12-14 17:01:44 -0300

    fix: requisito 2 3 e 4

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 window.onload = () => {
-    
+    standardColors();
+        
 }
-
 const colorPalete = document.getElementsByClassName('color');
 const colorButton = document.getElementById('button-random-color');
 const colorPaleteBlack = document.getElementsByClassName('color')[0];
 colorPaleteBlack.style.backgroundColor = 'black'
-let randomColor1 = Math.round(Math.random() * 255)
-let randomColor2 = Math.round(Math.random() * 255)
-let randomColor3 = Math.round(Math.random() * 255)
+const colors = ['black', 'red', 'green', 'purple']
 
-
-// function generateColorPalete() {
-//     for (let index = 0; index < colorPalete.length; index++) {
-//         colorPalete[index].style.backgroundColor = `rgb(${randomColor1}, ${randomColor2}, ${randomColor3})`
-//     }    
-// }
-
-
+function standardColors(){
+    for (let index = 0; index < colorPalete.length; index++) {
+        let fillPalete = ''
+        for (let pickColor = 0; pickColor <= colors.length; pickColor++) {
+            fillPalete = colors[index];
+        }
+    colorPalete[index].style.backgroundColor = fillPalete; 
+    }
+}
 
 function colorRandom(){
     for (let index = 1; index < colorPalete.length; index++) {
     colorPalete[index].style.backgroundColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}`;
-        localStorage.setItem('color', colorPalete[index])
     }
 }
 colorButton.addEventListener('click', colorRandom)
