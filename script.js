12c190160a00ec800ae0ad9c25c560813e2d744e
Julio Rugoloe <juliorugolo@gmail.com>
2022-12-14 18:08:06 -0300

    feat: requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 window.onload = () => {
-    standardColors();
-        
+  standardColors();
 }
+
 const colorPalete = document.getElementsByClassName('color');
 const colorButton = document.getElementById('button-random-color');
 const colorPaleteBlack = document.getElementsByClassName('color')[0];
-colorPaleteBlack.style.backgroundColor = 'black'
-const colors = ['black', 'red', 'green', 'purple']
+colorPaleteBlack.style.backgroundColor = 'black';
+const colors = ['black', 'red', 'green', 'purple'];
 
-function standardColors(){
-    for (let index = 0; index < colorPalete.length; index++) {
-        let fillPalete = ''
-        for (let pickColor = 0; pickColor <= colors.length; pickColor++) {
-            fillPalete = colors[index];
-        }
+function standardColors() {
+    for (let index = 0; index < colorPalete.length; index += 1) {
+      let fillPalete = ''
+      for (let pickColor = 0; pickColor <= colors.length; pickColor += 1) {
+        fillPalete = colors[index];
+      }
     colorPalete[index].style.backgroundColor = fillPalete; 
     }
 }
@@ -23,4 +23,23 @@ function colorRandom(){
     colorPalete[index].style.backgroundColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}`;
     }
 }
-colorButton.addEventListener('click', colorRandom)
+colorButton.addEventListener('click', colorRandom);
+
+const createMatriz = () => {
+    const matriz = document.querySelector('#pixel-board');
+    for(let index = 0; index < 5; index += 1){
+      const line = document.createElement('section');
+      line.className = 'line';
+      for(let index1 = 0; index1 < 5; index1 += 1){
+        const cell = document.createElement('div');
+        cell.className = 'pixel';
+        cell.style.backgroundColor = 'white'
+        cell.style.height = '40px'
+        cell.style.height = '40px'
+        line.appendChild(cell);
+      }
+      matriz.appendChild(line);
+    }
+  }
+  createMatriz();
+
