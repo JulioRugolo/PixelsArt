11ce95b98b049bbba6a7a28dfe61f81660e8c220
Julio Rugoloe <juliorugolo@gmail.com>
2022-12-18 18:40:11 -0300

    Corrigindo Lint

--- script.js
+++ script.js
@@ -10,35 +10,70 @@ const matriz = document.getElementById('matriz');
 const paintPixel = document.getElementsByClassName('pixel');
 const retrieve = JSON.parse(localStorage.getItem('colorPalette'));
 const rgb = () => Math.round(Math.random() * 255);
+const localBoardSize = parseInt(localStorage.getItem('boardSize'), 10);
+const boardSize = document.getElementById('board-size');
+const loadDrawArray = JSON.parse(localStorage.getItem('pixelBoard'));
 let selectedColor = 'rgb(0 , 0 , 0)';
 let numberOfPixels = 5;
-const localBoardSize = parseInt((localStorage.getItem('boardSize'), 10));
 
+/* FUNÇÕES */
+function saveBoardSize() {
+  localStorage.setItem('boardSize', numberOfPixels);
+}
+
+function between5and50() {
+  if (numberOfPixels < 5) {
+    numberOfPixels = 5;
+  }
+  if (numberOfPixels > 50) {
+    numberOfPixels = 50;
+  }
+}
+
+function saveDraw() {
+  const saveDrawArray = [];
+  for (let index = 0; index < paintPixel.length; index += 1) {
+    saveDrawArray.push(paintPixel[index].style.backgroundColor);
+    localStorage.setItem('pixelBoard', JSON.stringify(saveDrawArray));
+  }
+}
+
+function paint() {
+  for (let index = 0; index < paintPixel.length; index += 1) {
+    paintPixel[index].addEventListener('click', () => {
+      paintPixel[index].style.backgroundColor = selectedColor;
+      saveDraw();
+    });
+  }
+}
 
 /* Cores padrão */
-function standardColors() {
+function defineRetrievedColor() {
   for (let index = 0; index < colorPalete.length; index += 1) {
+    colors.push(retrieve[index]);
     colorPalete[index].style.backgroundColor = colors[index];
   }
-  retrieveColor()
 }
-standardColors();
-
 function retrieveColor() {
   for (let indexLocal = 0; indexLocal < localStorage.length; indexLocal += 1) {
     if (localStorage !== null && localStorage.key(indexLocal) === 'colorPalette') {
       colors = ['black'];
-      for (let index = 0; index < colorPalete.length; index += 1){
-        colors.push(retrieve[index]);
-        colorPalete[index].style.backgroundColor = colors[index];
-      }
+      defineRetrievedColor();
     }
   }
 }
 
+function standardColors() {
+  for (let index = 0; index < colorPalete.length; index += 1) {
+    colorPalete[index].style.backgroundColor = colors[index];
+  }
+  retrieveColor();
+}
+standardColors();
+
 /* Cor aleatória */
 function colorRandom() {
-  let randomColor = [];
+  const randomColor = [];
   for (let index = 1; index < colorPalete.length; index += 1) {
     colorPalete[index].style.backgroundColor = `rgb(${rgb()}, ${rgb()}, ${rgb()})`;
     randomColor.push(colorPalete[index].style.backgroundColor);
@@ -76,7 +111,7 @@ const createCollum = (cells) => {
     ul.appendChild(pixel);
     div.style.height = `${(parseInt(pixel.style.height, 10) * cells) + (cells * 2)}px`;
     div.style.width = `${(parseInt(pixel.style.height, 10) * cells) + (cells * 2)}px`;
-    paint()
+    paint();
   }
 };
 
@@ -93,10 +128,10 @@ const removePixels = () => {
   const pixelRemove = document.getElementsByClassName('pixel');
   while (pixelRemove.length > 0) {
     for (let index = 0; index < pixelBoardUl.length; index += 1) {
-      pixelBoardUl[index].remove()
+      pixelBoardUl[index].remove();
     }
   }
-}
+};
 
 /* SELECT COLOR */
 function removeAllClass() {
@@ -109,7 +144,7 @@ const colorSelect = (event) => {
   if (event.target.className === 'color') {
     removeAllClass();
     event.target.classList.add('selected');
-    return selectedColor = event.target.style.backgroundColor;
+    selectedColor = event.target.style.backgroundColor;
   }
 };
 
@@ -118,14 +153,6 @@ for (let index = 0; index < colorPalete.length; index += 1) {
 }
 
 /* PAINT GRID */
-function paint() {
-  for (let index = 0; index < paintPixel.length; index += 1) {
-    paintPixel[index].addEventListener('click', (event) => {
-      event.target.style.backgroundColor = selectedColor;
-      saveDraw();
-    });
-  }
-}
 
 /* CLEAR BOARD */
 const clearBoard = document.getElementById('clear-board');
@@ -133,17 +160,14 @@ clearBoard.addEventListener('click', () => {
   for (let index = 0; index < paintPixel.length; index += 1) {
     paintPixel[index].style.backgroundColor = 'white';
     localStorage.clear();
-    location.reload();
   }
 });
 
 /* INPUT FUNCTION */
-let boardSize = document.getElementById('board-size');
-
 function inputValidate() {
   if (boardSize.value > 0) {
     numberOfPixels = boardSize.value;
-    between5and50()
+    between5and50();
     removePixels();
     createLines(numberOfPixels);
     saveBoardSize();
@@ -152,53 +176,32 @@ function inputValidate() {
   }
 }
 
-function between5and50() {
-  if (numberOfPixels < 5) {
-    numberOfPixels = 5;
-  }
-  if (numberOfPixels > 50) {
-    numberOfPixels = 50;
-  }
-}
 /* BOTÃO GERAR NOVA PIXELBOARD */
 const botaoVQV = document.getElementById('generate-board');
-botaoVQV.addEventListener('click', (event) => {
-  inputValidate()
+botaoVQV.addEventListener('click', () => {
+  inputValidate();
+  paintPixel.addEventListener('click', () => {
   for (let index = 0; index < paintPixel.length; index += 1) {
-    paintPixel[index].addEventListener('click', (event) => {
-      event.target.style.backgroundColor = selectedColor;
-    })
-  };
-})
+    paintPixel.style.backgroundColor = selectedColor;
+    }
+  });
+});
 
 /* LOCAL STORAGE */
-function saveBoardSize() {
-  localStorage.setItem('boardSize', numberOfPixels)
-}
-
-function saveDraw() {
-  let  saveDrawArray = [];
-  for (let index = 0; index < paintPixel.length; index += 1) {
-    saveDrawArray.push(paintPixel[index].style.backgroundColor);
-    localStorage.setItem('pixelBoard', JSON.stringify(saveDrawArray));
+function paintingOldDraw() {
+  for (let indexPaint = 0; indexPaint < loadDrawArray.length; indexPaint += 1) {
+    paintPixel[indexPaint].style.backgroundColor = loadDrawArray[indexPaint];
   }
 }
-
 function loadDraw() {
-  let loadDrawArray = JSON.parse(localStorage.getItem('pixelBoard'));
-  for (let index = 0; index < localStorage.length; index++) {
+  for (let index = 0; index < localStorage.length; index += 1) {
     if (localStorage.length > 0 && localStorage.key(index) === 'pixelBoard') {
-      for (let indexPaint = 0; indexPaint < loadDrawArray.length; indexPaint += 1) {
-        paintPixel[indexPaint].style.backgroundColor = loadDrawArray[indexPaint];
-      }
-    }
+      paintingOldDraw();
   }
+}
 }
 loadDraw();
 
-
-
-
 window.onload = () => {
-  colorPaleteBlack.className += ' selected';
+ colorPaleteBlack.className += ' selected';
 };
